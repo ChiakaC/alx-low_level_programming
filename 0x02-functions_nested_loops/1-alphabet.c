34dc5fda89e5d62264cb1d82a5de7024475492c3
ChiakaC <regiumservices@gmail.com>
2022-09-29 04:38:48 -0700

    commit for task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,9 @@
  * main - entry point
  * Description: program that prints _putchar
  * Return: 0
-*/
+ */
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-										return (0);
+	return (0);
 }
